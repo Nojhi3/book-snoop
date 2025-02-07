ef633e3a536f4c1b92dade1e96b38d4c24707108
Akshat <116896690+Nojhi3@users.noreply.github.com>
2025-02-07 23:17:28 +0530

    allow all hosts

--- book_store/settings.py
+++ book_store/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-6j(y!0-(4pbuj(q3gnxz-0tl+0st3cxt(yp=#&4sf0fa1yx)28
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
